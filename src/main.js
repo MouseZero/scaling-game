50a36f70ad2a549da826c1c8d000f1ebee94cac9
Russell Murray <thunderbird>
2016-10-14 12:04:28 -0700

    refactor

--- src/main.js
+++ src/main.js
@@ -1,34 +1,11 @@
-const createCelestialBody = require('./scripts/createCelestialBody')
-const scaler = require('./scripts/scaler')
-const CANVAS_SIZE = 1000
 const createMenu = require('./scripts/menu')
+const canvasDisplay = require('./scripts/gameScene').canvasDisplay
+const gameState = {
+  compareSceneRunning: false
+}
 
 window.init = function () {
   const celestialBodyData = require('./data/data')
   createMenu(celestialBodyData)
   canvasDisplay(celestialBodyData)
-}
-
-function canvasDisplay(celestialBodyData){
-  const bodies = createBodies(celestialBodyData)
-  const stage = new createjs.Stage('game')
-  placeBodies(stage, bodies)
-  stage.update()
-}
-
-function createBodies(celestialBodyData){
-  const selection = [
-    celestialBodyData.rulers[1],
-    celestialBodyData.rulers[0],
-    celestialBodyData.showcase[3]
-  ]
-  return createCelestialBody(selection, CANVAS_SIZE)
-}
-
-function placeBodies(stage, bodies){
-  bodies.forEach(function (elem){
-    elem.x = 500
-    elem.y = 500
-    stage.addChild(elem)
-  })
 }
---
+++ src/scripts/gameScene.js
@@ -0,0 +1,31 @@
+const createCelestialBody = require('./createCelestialBody')
+const CANVAS_SIZE = 1000
+
+function canvasDisplay(celestialBodyData){
+  const stage = new createjs.Stage('game')
+  postNewScene(stage, celestialBodyData, 2)
+  stage.update()
+}
+
+function postNewScene(stage, celestialBodyData, showcaseBodyId){
+  const bodies = createBodies(celestialBodyData, showcaseBodyId)
+  placeBodies(stage, bodies)
+}
+
+function createBodies(celestialBodyData, showcaseBodyId){
+  const selection = [
+    ...celestialBodyData.rulers,
+    celestialBodyData.showcase[showcaseBodyId]
+  ]
+  return createCelestialBody(selection, CANVAS_SIZE)
+}
+
+function placeBodies(stage, bodies){
+  bodies.forEach(function (elem){
+    elem.x = 500
+    elem.y = 500
+    stage.addChild(elem)
+  })
+}
+
+module.exports.canvasDisplay = canvasDisplay
