d24063bf9c047d930235c1c6a3efff55ee4b7f8c
Russell Murray <thunderbird>
2016-10-13 22:45:24 -0700

    refactor

--- src/main.js
+++ src/main.js
@@ -5,20 +5,10 @@ const CANVAS_SIZE = 1000
 window.init = function () {
   const data = require('./data/data')
   const stage = new createjs.Stage('game')
-  const selection = [data.rulers[1], data.rulers[0], data.showcase[2]]
-  const bodies = selection.map(function (elem) {
-    return createCelestialBody(elem, maxBodySize(selection), CANVAS_SIZE)
-  }).sort(compareDataSize)
-
+  const selection = [data.rulers[1], data.rulers[0], data.showcase[3]]
+  const bodies = createAllBodies(selection, CANVAS_SIZE)
   console.log(bodies)
-
-
-  bodies.forEach(function (elem){
-    elem.x = 500
-    elem.y = 500
-    console.log(elem.solarSize)
-    stage.addChild(elem)
-  })
+  placeBodies(stage, bodies)
 
   stage.update()
 
@@ -33,4 +23,19 @@ function maxBodySize(bodies) {
 function compareDataSize(a, b){
   console.log('called compare')
   return b.solarSize - a.solarSize
+}
+
+function createAllBodies(selection, canvasSize){
+  return selection.map(function (elem) {
+    return createCelestialBody(elem, maxBodySize(selection), CANVAS_SIZE)
+  }).sort(compareDataSize)
+}
+
+function placeBodies(stage, bodies){
+  bodies.forEach(function (elem){
+    elem.x = 500
+    elem.y = 500
+    console.log(elem.solarSize)
+    stage.addChild(elem)
+  })
 }
