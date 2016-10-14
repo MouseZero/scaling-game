602cfdc5c1b448f5cf3c703dfee3309aa958183b
Russell Murray <thunderbird>
2016-10-13 17:26:35 -0700

    Canvas now scales with window size

--- src/main.js
+++ src/main.js
@@ -4,17 +4,9 @@ window.init = function () {
   const data = require('./data/data')
   const stage = new createjs.Stage('game')
   const sun = createCelestialBody(data.rulers[0])
-  sun.x = 100
-  sun.y = 100
+  sun.x = 500
+  sun.y = 500
   stage.addChild(sun)
 
   stage.update()
-}
-
-function drawACircle(stage){
-  const circle = new createjs.Shape()
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50)
-  circle.x = 100
-  circle.y = 100
-  stage.addChild(circle)
 }
--- src/scripts/createCelestialBody.js
+++ src/scripts/createCelestialBody.js
@@ -1,6 +1,6 @@
 module.exports = function (bodyInfo) {
   const body = new createjs.Shape()
-  body.graphics.beginFill(bodyInfo.color).drawCircle(0, 0, 50)
+  body.graphics.beginFill(bodyInfo.color).drawCircle(0, 0, 300)
   body.solarSize = bodyInfo.size
   return body
 }
