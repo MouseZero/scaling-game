ddaa70f13bd8f41e337d5fb79ee4a1f78b5fd314
Russell Murray <thunderbird>
2016-10-13 22:51:44 -0700

    refactor

--- src/main.js
+++ src/main.js
@@ -3,39 +3,22 @@ const scaler = require('./scripts/scaler')
 const CANVAS_SIZE = 1000
 
 window.init = function () {
-  const data = require('./data/data')
+  const bodies = createBodies()
   const stage = new createjs.Stage('game')
-  const selection = [data.rulers[1], data.rulers[0], data.showcase[3]]
-  const bodies = createAllBodies(selection, CANVAS_SIZE)
-  console.log(bodies)
   placeBodies(stage, bodies)
-
   stage.update()
-
 }
 
-function maxBodySize(bodies) {
-  return bodies.reduce(function (prev, elem) {
-    return (prev > elem.size) ? prev : elem.size
-  }, 0)
-}
-
-function compareDataSize(a, b){
-  console.log('called compare')
-  return b.solarSize - a.solarSize
-}
-
-function createAllBodies(selection, canvasSize){
-  return selection.map(function (elem) {
-    return createCelestialBody(elem, maxBodySize(selection), CANVAS_SIZE)
-  }).sort(compareDataSize)
+function createBodies(){
+  const data = require('./data/data')
+  const selection = [data.rulers[1], data.rulers[0], data.showcase[3]]
+  return createCelestialBody(selection, CANVAS_SIZE)
 }
 
 function placeBodies(stage, bodies){
   bodies.forEach(function (elem){
     elem.x = 500
     elem.y = 500
-    console.log(elem.solarSize)
     stage.addChild(elem)
   })
 }
--- src/scripts/createCelestialBody.js
+++ src/scripts/createCelestialBody.js
@@ -1,9 +1,25 @@
 const scaler = require('./scaler')
 
-module.exports = function (bodyInfo, maxSize, canvasSize) {
+module.exports = function(selection, canvasSize){
+  return selection.map(function (elem) {
+    return createACelestialBody(elem, maxBodySize(selection), canvasSize)
+  }).sort(compareDataSize)
+}
+
+function createACelestialBody(bodyInfo, maxSize, canvasSize) {
   const body = new createjs.Shape()
   const size = scaler.solarSizeCompare(bodyInfo.size, maxSize, canvasSize)
   body.graphics.beginFill(bodyInfo.color).drawCircle(0, 0, size)
   body.solarSize = bodyInfo.size
   return body
+}
+
+function maxBodySize(bodies) {
+  return bodies.reduce(function (prev, elem) {
+    return (prev > elem.size) ? prev : elem.size
+  }, 0)
+}
+
+function compareDataSize(a, b){
+  return b.solarSize - a.solarSize
 }
