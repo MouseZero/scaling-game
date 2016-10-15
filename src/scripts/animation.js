c5a5befc5700ce2911a620298f307ddd1b1cc3b1
mousezero <rm@mousezero.com>
2016-10-15 16:04:10 -0700

    Started promises for animation hand off

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -2,20 +2,31 @@ const createjs = require('createjs-collection')
 const scaler = require('./scaler')
 
 function sizeCompareAnimation (canvasSize, shapes) {
-  const multipler = scaler.solarMultiplier(canvasSize, shapes[0].solarSize)
-  shapes[0].scaleX = multipler
-  shapes[0].scaleY = multipler
-
   const mult2 = scaler.solarMultiplier(canvasSize, shapes[1].solarSize)
 
-  createjs.Tween.get(shapes[0], { loop: false })
-    .to({x: 500, y: 500}, 2000, createjs.Ease.getPowInOut(4))
-    .wait(1000)
-    .call(introAnimation, [shapes[1], mult2], this)
-    .to({
-      scaleX: mult2,
-      scaleY: mult2
-    }, 3000, createjs.Ease.getPowOut(4))
+  startAnimation(canvasSize, shapes)
+    .then(function () {
+      introAnimation(shapes[1], mult2)
+    })
+}
+
+function startAnimation (canvasSize, shapes) {
+  return new Promise(function (resolve, reject) {
+    const multipler = scaler.solarMultiplier(canvasSize, shapes[0].solarSize)
+    shapes[0].scaleX = multipler
+    shapes[0].scaleY = multipler
+    const mult2 = scaler.solarMultiplier(canvasSize, shapes[1].solarSize)
+
+    createjs.Tween.get(shapes[0], { loop: false })
+      .to({x: 500, y: 500}, 2000, createjs.Ease.getPowInOut(4))
+      .wait(1000)
+      .call(resolve, [], this)
+      .to({
+        scaleX: mult2,
+        scaleY: mult2
+      }, 3000, createjs.Ease.getPowOut(4))
+  })
+
 }
 
 function introAnimation (shape, currentScale) {
