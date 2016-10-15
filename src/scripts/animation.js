8a4f702413af147c40621182b2daf468ed26e4df
mousezero <rm@mousezero.com>
2016-10-15 02:46:54 -0700

    Numbers for scaling seem to be in order

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -9,22 +9,18 @@ function fallIn (shape) {
 function sizeCompareAnimation (canvasSize, shapes) {
   // Old animation
   shapes.forEach(e => fallIn(e))
-  shapes.forEach(e => console.log(e.solarSize))
 
   // New Animation
   const solarSizes = shapes.map(x => x.solarSize)
   const sizes = scaler.sizeForAllBodies(canvasSize, solarSizes)
   const homePos = getHomePositions(canvasSize, sizes)
 
-  console.log(sizes)
-  console.log(homePos)
-
   createjs.Tween.get(shapes[0], { loop: false })
     .to(homePos[0][0], 2000, createjs.Ease.getPowInOut(4))
 }
 
 function getHomePositions (canvasSize, sizes) {
-  return sizes.reverse().reduce((prev, _, i, all) => {
+  return sizes.reduce((prev, _, i, all) => {
     const subSet = all.slice(0, i + 1)
     const homePos = scaler.homePositionForAllBodies(canvasSize, subSet)
     return [...prev, homePos]
