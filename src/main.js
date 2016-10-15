c71ac5c6ea1cc7f8f4ae8dd3f6829aa755b65d0d
mousezero <rm@mousezero.com>
2016-10-15 12:58:01 -0700

    Refactor "no code is the best code"

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ const celestialBodyData = require('./data/data')
 
 window.init = function () {
   // -- Menu --
-  const s = require('./scripts/scaler')
-  s.sizeForAllBodies(1000, [5, 6])
   createMenu(celestialBodyData, newScene)
 
   // -- Graphics Display --
--- src/scripts/scaler.js
+++ src/scripts/scaler.js
@@ -1,43 +1,5 @@
-const curry = require('lodash.curry')
-const MARGIN = 0.1
-const MARGIN_INVERT = 1 - MARGIN
-
-function solarSizeCompare (canvasSize, largestBodySize, currentBodySize) {
-  return (currentBodySize * scale(canvasSize, largestBodySize))
-}
-
-function scale (canvasSize, largestSize) {
-  return (((canvasSize) / largestSize) * 0.5) * MARGIN_INVERT
-}
-
-function sizeForAllBodies (canvasSize, solarSizes) {
-  const scaleNum = scale(canvasSize, Math.max(...solarSizes))
-  return solarSizes.map(x => x * scaleNum)
-}
-
-function bodyHomePosition (canvasSize, largestBodySize, currentBodySize) {
-  const sizeOnCanvas = solarSizeCompare(canvasSize, largestBodySize, currentBodySize)
-  const x = (canvasSize * (1 - MARGIN_INVERT)) + sizeOnCanvas
-  const y = (canvasSize - sizeOnCanvas) - (canvasSize * MARGIN) / 2
-  return { x, y }
-}
-
-function homePositionForAllBodies (canvasSize, bodySizes) {
-  const findPos = curry(bodyHomePosition)(canvasSize, Math.max(...bodySizes))
-  return bodySizes.map(x => findPos(x))
-}
-
-function creationSize (canvasSize, solarSize) {
-  return (solarSize / 100) * canvasSize
-}
-
 function solarMultiplier (canvasSize, largestSize) {
   return canvasSize / largestSize
 }
 
-module.exports.solarSizeCompare = solarSizeCompare
-module.exports.bodyHomePosition = bodyHomePosition
-module.exports.sizeForAllBodies = sizeForAllBodies
-module.exports.homePositionForAllBodies = homePositionForAllBodies
-module.exports.creationSize = creationSize
 module.exports.solarMultiplier = solarMultiplier
