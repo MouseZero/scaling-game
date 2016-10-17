df1b7096093e4685bc25f0dc390e95faf5b1a888
mousezero <rm@mousezero.com>
2016-10-17 01:01:14 -0700

    Refactor

--- src/public/scripts/gameScene.js
+++ src/public/scripts/gameScene.js
@@ -2,20 +2,19 @@ const createCelestialBody = require('./createCelestialBody')
 const createjs = require('createjs-collection')
 const animation = require('./animation')
 const TICKER_NAME = 'tick'
-var CANVAS_SIZE = 1000
 
 function newScene (stage, canvasSize, celestialBodyData) {
-  CANVAS_SIZE = canvasSize
   const bodies = createCelestialBody(canvasSize, celestialBodyData.bodies)
-  placeBodies(stage, bodies.slice().reverse())
-  animation.sizeCompareAnimation(stage, CANVAS_SIZE, bodies)
+  placeBodies(stage, bodies)
+  animation.sizeCompareAnimation(stage, canvasSize, bodies)
 
   createjs.Ticker.setFPS(30)
   createjs.Ticker.addEventListener(TICKER_NAME, stage)
 }
 
 function placeBodies (stage, bodies) {
-  bodies.forEach(x => stage.addChild(x))
+  bodies.slice().reverse()
+    .forEach(x => stage.addChild(x))
 }
 
 module.exports.newScene = newScene
